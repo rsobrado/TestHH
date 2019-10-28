a0aca58f817ec71e36f3da30ebed7306cdc86043
Roger Sobrado <roger.sobrado.e@gmail.com>
2019-10-28 08:26:23 -0600

    slides

--- src/components/SlideFourth.js
+++ src/components/SlideFourth.js
@@ -32,14 +32,22 @@ const SlideFourth  = () => {
               <div className="hero-item">
                 <h3>Nam dolor massa, vulputate</h3>
                 <p>Pellentesque ut sem vel metus ornare aliquam vel ut neque. Suspendisse ac ipsum nec ipsum ultricies porta.</p>
+
+                <em className="">Fusce in efficitur <NavLink href="tel:555-555-5555">555-555-5555</NavLink> Curabitur viverra, molestie tincidunt.</em>
+                  <Button>Donec velita </Button>
               </div>
               <div className="hero-item">
                 <h3>Sed vestibulum odiosem, sit ametr</h3>
-                <p>Molestie, congue leo ac, bibendum dui. Pellentesque ut sem vel metus ornare aliquam vel ut neque. </p>
+                <p>Fusce non sodales odio, sit amet dignissim ipsum. Pellentesque eleifend felis augue.</p>
+                <p> 
+                  
+                </p>
               </div>
               <div className="hero-item">
                 <h3>Nullam ac convallis diam, eu prellfua</h3>
                 <p>Vel metus ornare aliquam vel ut neque. Suspendisse ac ipsum nec ipsum ultricies porta. Praesent ut ornare lacus.  Pellentesque non tortor vel erat feugiat.</p>
+                <p className="small">Vivamus vitae urna nisi. Suspendisse ex nunc, ornare eget consectetur sit amet eget consectetur sit amet.</p>
+                <em className="">Fusce in efficitur <NavLink href="tel:555-555-5555">555-555-5555</NavLink> Curabitur viverra, molestie tincidunt.</em>
               </div>
             </div>
             </Col>
