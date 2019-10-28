03f954dd4c23a066c7c6beb91e8f562dd9706b1b
Roger Sobrado <roger.sobrado.e@gmail.com>
2019-10-28 10:34:49 -0600

    separator images added

---
+++ src/components/ContentSeparator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './ContentSeparator.scss';
+import { ReactComponent as Savings}  from '../images/savings.svg';
+import Background from '../images/savings-separator.jpg';
+
+
+import {
+  Container,
+  NavLink
+ } from 'reactstrap';
+
+
+const ContentSeparator = (props) => {
+  // const isLoggedIn = props.customClass;
+
+  return (
+    
+
+    // <div className={"ContentSeparator" + props.customClass}>
+    <div className="ContentSeparator" >
+        <Container fluid={true} >
+            <Container >
+                <div className="flex">
+                    <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+                    <Savings/>
+                </div>
+            </Container>
+        </Container>
+    </div>
+  );
+}
+
+export default ContentSeparator;
